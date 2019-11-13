fc153b3008efe7e2d615d774ee4fe9c8c8b55065
mohabashraf <mohabashraf466@gmail.com>
2019-11-13 05:32:59 +0200

    add integration test setup

--- src/test/java/com/cart_service/CartServiceApplicationTests.java
+++ src/test/java/com/cart_service/CartServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.cart_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class CartServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
